7e5495d7851d257307545eaae7946bcbc053f6bc
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-05-31 14:02:01 +0200

    Redirect if connected

--- app/Http/Controllers/HomePublicController.php
+++ app/Http/Controllers/HomePublicController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class HomePublicController extends Controller
 {
-    public function __invoke(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\Foundation\Application
+    public function __invoke(): \Illuminate\Contracts\View\View|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
+        if (auth()->user()) {
+            return redirect()->route('dashPublic.index');
+        }
         return view('public/auth/index');
     }
 }
