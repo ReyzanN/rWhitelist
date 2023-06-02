a4cd65c6140bd5da702aebab14865077860fa7e5
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-06-02 15:14:13 +0200

    Update Style Dashboard

---
+++ resources/views/layouts/public/nav.blade.php
@@ -0,0 +1,38 @@
+<div class="row col-3 bg-black rounded" id="navBloc">
+    <div class="d-flex justify-content-center align-content-center">
+        <img src="/img/dashLogo.png" alt="logo" class="imgLogo">
+    </div>
+    <div class="">
+        <div class="row d-flex justify-items-center align-items-center">
+            <ul class="text-center poppins">
+                <li class="list-group-item mt-2">
+                    <a href="#" class="link-underline link-underline-opacity-0 text-white HoverLinkCustomColor"><i class="bi bi-house"></i>&nbsp;Accueil</a>
+                </li>
+                <li class="list-group-item mt-2">
+                    <a href="#" class="link-underline link-underline-opacity-0 text-white HoverLinkCustomColor"><i class="bi bi-person"></i>&nbsp;Mon Profil</a>
+                </li>
+                <li class="list-group-item mt-2">
+                    <a href="#" class="link-underline link-underline-opacity-0 text-white HoverLinkCustomColor"><i class="bi bi-info-circle"></i>&nbsp;Informations</a>
+                </li>
+                <li class="list-group-item mt-2">
+                    <a href="#" class="link-underline link-underline-opacity-0 text-white HoverLinkCustomColor"><i class="bi bi-chat-left"></i>&nbsp;Mes demandes</a>
+                </li>
+            </ul>
+        </div>
+        <div class="row mt-5"></div>
+        <div class="row">
+            <p class="text-uppercase text-light text-opacity-75 text-center">whitelist</p>
+            <ul class="text-center poppins">
+                <li class="list-group-item">
+                    <a href="#" class="link-underline link-underline-opacity-0 text-white HoverLinkCustomColor"><i class="bi bi-patch-question"></i>&nbsp;QCM</a>
+                </li>
+                <li class="list-group-item mt-2">
+                    <a href="#" class="link-underline link-underline-opacity-0 text-white HoverLinkCustomColor"><i class="bi bi-clock"></i>&nbsp;Sessions</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="d-flex justify-content-end align-content-end justify-items-end align-items-end poppins">
+        <a href="{{ route('auth.logout') }}" class="link-underline link-underline-opacity-0 text-light text-opacity-50 HoverLinkCustomColor"><i class="bi bi-box-arrow-left"></i>&nbsp;Déconnexion</a>
+    </div>
+</div>
--- resources/views/public/dashboard/index.blade.php
+++ resources/views/public/dashboard/index.blade.php
@@ -5,18 +5,83 @@
 @section('content')
     <div class="container-fluid top10vh text-white">
         <div class="row-cols-12 d-flex flex-row d-flex justify-content-center">
-            <div class="row col-3 bg-black rounded">
-                <div class="row d-flex justify-content-center align-content-center">
-                    <img src="/img/dashLogo.png" alt="logo" class="imgLogo">
+            @include('layouts.public.nav')
+            <div class="row col-9 d-flex justify-content-around poppins">
+                <div class="col-3 bg-black rounded BlocIndexDashBoard">
+                    <div class="row">
+                        <div class="col-9 text-white">
+                            <p class="text-capitalize mt-1 fw-bold">Étapes whitelist</p>
+                        </div>
+                        <div class="col-3 text-light text-opacity-75">
+                            <p class="text-capitalize mt-1">Status</p>
+                        </div>
+                    </div>
+                    <div class="row mt-2"></div>
+                    <div class="row">
+                        <div class="row mt-3">
+                            <div class="col-9">Questionnaire</div>
+                            <div class="col-3 text-center">NON</div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-9">Entretient</div>
+                            <div class="col-3 text-center">OUI</div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-9">Status Whitelist</div>
+                            <div class="col-3 text-center">NON</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="row mt-3">
-                    NAVIGATION
+                <div class="col-3 bg-black rounded BlocIndexDashBoard">
+                    <div class="row">
+                        <div class="col-9 text-white">
+                            <p class="text-capitalize mt-1 fw-bold">Étapes whitelist</p>
+                        </div>
+                        <div class="col-3 text-light text-opacity-75">
+                            <p class="text-capitalize mt-1">Status</p>
+                        </div>
+                    </div>
+                    <div class="row mt-2"></div>
+                    <div class="row">
+                        <div class="row mt-3">
+                            <div class="col-9">Questionnaire</div>
+                            <div class="col-3 text-center">NON</div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-9">Entretient</div>
+                            <div class="col-3 text-center">OUI</div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-9">Status Whitelist</div>
+                            <div class="col-3 text-center">NON</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="row col-9 d-flex justify-content-around">
-                <div class="col-3 bg-black rounded">BLOC 1</div>
-                <div class="col-3 bg-black rounded">BLOC 2</div>
-                <div class="col-3 bg-black rounded">BLOC 3</div>
+                <div class="col-3 bg-black rounded BlocIndexDashBoard">
+                    <div class="row">
+                        <div class="col-9 text-white">
+                            <p class="text-capitalize mt-1 fw-bold">Étapes whitelist</p>
+                        </div>
+                        <div class="col-3 text-light text-opacity-75">
+                            <p class="text-capitalize mt-1">Status</p>
+                        </div>
+                    </div>
+                    <div class="row mt-2"></div>
+                    <div class="row">
+                        <div class="row mt-3">
+                            <div class="col-9">Questionnaire</div>
+                            <div class="col-3 text-center">NON</div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-9">Entretient</div>
+                            <div class="col-3 text-center">OUI</div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-9">Status Whitelist</div>
+                            <div class="col-3 text-center">NON</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
